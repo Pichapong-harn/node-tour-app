27ee37397b233977e5b4a2a311b88d7e8f1fd0b1
penguinkanong <penguinkanong@github.com>
2020-05-18 16:21:12 +0700

    first api

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({message:'Server side work.', app: "Natours"});
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-    res.send('You can now post...')
-})
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
 const port = 3000;
 app.listen(port, () => {
